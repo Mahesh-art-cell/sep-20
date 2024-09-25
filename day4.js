f12844cf61d4268abe0ffa5b506a1a40de25afa9
Mahesh-art-cell <tallurim886@email.com>
2024-09-25 20:05:47 +0530

    the file is added

---
+++ day4.js
@@ -0,0 +1,42 @@
+var a = ["nanna","amma","me","brother"]
+console.log(a);
+a[0]="murthy";
+a[1]="koteswaramma";
+console.log(a);//modification
+a[4]="babai";
+a[5]="pinni";
+console.log(a)//adding
+
+var b=100;
+var b1=b.toString
+console.log(typeof b1 , b1);
+var a = 134.46553;
+console.log(parseFloat(a));
+console.log(isFinite(a));
+console.log(a.toFixed());
+console.log(a.toFixed(3));
+console.log(a.toFixed(5));
+console.log(a.toPrecision());
+console.log(a.toPrecision(3));
+
+var a=123567;
+console.log(a.toExponential());
+console.log(a.toExponential(1));
+console.log(a.toExponential(2));
+console.log(a.toExponential(3));
+console.log(a.toExponential(4));
+console.log(a.toExponential(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
